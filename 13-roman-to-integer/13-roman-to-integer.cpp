bcd4ef15a7beab2fb8b0f184e9d695b3bc3ed0dc
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2022-09-28 23:34:58 +0530

    Time: 40 ms (12.57%), Space: 8.9 MB (8.45%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -3,17 +3,36 @@ public:
     int romanToInt(string s)
 {
         
-      unordered_map<char,int> ns{
-        {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000},
-    };
-    int ans = 0;
-    for(int i = 0; i<s.size(); i++){
-        if(ns[s[i]]<ns[s[i+1]])
-            ans -= ns[s[i]];
-        else
-            ans += ns[s[i]];
-    }
-    return ans;
+//       unordered_map<char,int> ns{
+//         {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000},
+//     };
+//     int ans = 0;
+//     for(int i = 0; i<s.size(); i++){
+//         if(ns[s[i]]<ns[s[i+1]])
+//             ans -= ns[s[i]];
+//         else
+//             ans += ns[s[i]];
+//     }
+//     return ans;
     
-}
+//}
+        
+
+          unordered_map<char,
+        int>m1={
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+
+    int ans=0;
+    for(int i=s.length();i>=0;i--)
+        ((i+1)<s.length() and m1[s[i]]<m1[s[i+1]])?ans-=m1[s[i]]:ans+=m1[s[i]];
+    
+    return ans;
+    }
 };
