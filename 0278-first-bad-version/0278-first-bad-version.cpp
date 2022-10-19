68c97a50cdc4afaedcf80f2a2ea41bcd6cf9f035
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2022-10-19 22:46:04 +0530

    Time: 2 ms (44.74%), Space: 5.8 MB (88.61%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -1,41 +1,41 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
-class Solution {
-    public:
-    int firstBadVersion(int n) {
-        long long int beg,last,mid;
-        beg = 1 , last = n;
-        long int pos = 1;
-        while(beg<=last){
-            mid = beg + (last-beg)/2;
-            bool x = isBadVersion(mid);
-            if(x == true){
-                pos = mid;
-                last = mid-1;
-            }
-            else
-                beg = mid+1;
-        }
-
-        return pos;
-    }
-};
-
-// bool isBadVersion(int version);
-
 // class Solution {
-// public:
+//     public:
 //     int firstBadVersion(int n) {
-//         int left = 1, right = n;
-//         while (left <= right) {
-//             int mid = left + (right - left) / 2;
-//             if (isBadVersion(mid)) {
-//                 right = mid - 1;
-//             } else {
-//                 left = mid + 1;
+//         long long int beg,last,mid;
+//         beg = 1 , last = n;
+//         long int pos = 1;
+//         while(beg<=last){
+//             mid = beg + (last-beg)/2;
+//             bool x = isBadVersion(mid);
+//             if(x == true){
+//                 pos = mid;
+//                 last = mid-1;
 //             }
+//             else
+//                 beg = mid+1;
 //         }
-//         return left;
+
+//         return pos;
 //     }
 // };
+
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
