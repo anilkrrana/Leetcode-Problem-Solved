dcc468e40c73710c78c87fb7780812d200fe1f88
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2022-11-03 22:56:13 +0530

    Time: 7 ms (52.19%), Space: 7.5 MB (5.72%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,17 @@
+// class Solution {
+// public:
+//     int myAtoi(string s) {
+        
+//     }
+// };
+
+class Solution {
+public:
+    int myAtoi(string str) {
+        int r = 0;
+        stringstream ss;
+        ss << str;
+        ss >> r;
+        return r;
+    }
+};
