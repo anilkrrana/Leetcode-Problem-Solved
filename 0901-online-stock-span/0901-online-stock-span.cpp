cf25a6a395045e16d14bdcf373aac81a782360bb
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2022-11-09 20:35:15 +0530

    Time: 469 ms (59.00%), Space: 84.1 MB (98.88%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,50 +1,50 @@
-// class StockSpanner {
-//     stack<pair<int, int>> storage;
-//     int index = 0;
-// public:
-//     StockSpanner() {
-        
-//     }
-    
-//     int next(int price) {
-//         if (storage.empty()) {
-//             storage.push({price, index++});
-//             return 1;
-//         } else {
-//             while (!storage.empty() && price >= storage.top().first) {
-//                 storage.pop();
-//             }
-//             int res = 0;
-//             res = storage.empty() ? index + 1 : index - storage.top().second;
-//             storage.push({price, index++});
-//             return res;
-//         }
-//     }
-// };
-
-
-
 class StockSpanner {
+    stack<pair<int, int>> stk;
+    int index = 0;
 public:
-    stack<int> storage;
-    vector<int> vec;
-    int i=0;
-    
     StockSpanner() {
         
     }
     
     int next(int price) {
-        vec.push_back(price);
-        int res;
-        while(!storage.empty() && price>=vec[storage.top()]) storage.pop();
-        
-        res = storage.empty() ? (i+1) : (i-storage.top());
-      
-        storage.push(i++);
-        return res;
+        if (stk.empty()) {
+            stk.push({price, index++});
+            return 1;
+        } else {
+            while (!stk.empty() && price >= stk.top().first) {
+                stk.pop();
+            }
+            int res = 0;
+            res = stk.empty() ? index + 1 : index - stk.top().second;
+            stk.push({price, index++});
+            return res;
+        }
     }
 };
+
+
+
+// class StockSpanner {
+// public:
+//     stack<int> storage;
+//     vector<int> vec;
+//     int i=0;
+    
+//     StockSpanner() {
+        
+//     }
+    
+//     int next(int price) {
+//         vec.push_back(price);
+//         int res;
+//         while(!storage.empty() && price>=vec[storage.top()]) storage.pop();
+        
+//         res = storage.empty() ? (i+1) : (i-storage.top());
+      
+//         storage.push(i++);
+//         return res;
+//     }
+// };
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
