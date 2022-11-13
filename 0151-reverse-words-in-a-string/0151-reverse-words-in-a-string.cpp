8e1159a79a397ec8a12a9fb76561e9cb9c4cc259
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2022-11-13 23:15:55 +0530

    Time: 10 ms (66.39%), Space: 8.1 MB (50.04%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,24 +1,54 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+	
+// 	    // Used for breaking words
+//         stringstream i(s);
+		
+// 		// To store individual words
+//         string word ; 
+		
+//         string ans = "" ; 
+//         while(i>>word) 
+//         {
+//             string tmp = word + " " + ans ;
+//             ans = tmp ; 
+//         }
+//         return ans.substr(0,ans.size()-1);    // removes the last space character
+//     }
+// };      
+            
+			
 class Solution {
 public:
     string reverseWords(string s) {
-	
-	    // Used for breaking words
-        stringstream i(s);
-		
-		// To store individual words
-        string word ; 
-		
-        string ans = "" ; 
-        while(i>>word) 
-        {
-            string tmp = word + " " + ans ;
-            ans = tmp ; 
+        stack<string> st ;
+        string str = "";
+        for(int i=0 ;i<s.size(); i++){
+            if(s[i]!=' '){
+                str+= s[i];
+                if(i== s.size()-1){
+                    st.push(str);
+                    st.push(" ");
+                }
+            }else{
+                if(str == ""){
+                    continue ;
+                }
+                st.push(str);
+                st.push(" ");
+                str = "";
+            }
         }
-        return ans.substr(0,ans.size()-1);    // removes the last space character
+        st.pop(); // removing extra space at the end 
+        string ans = "";
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        return ans ;
     }
-};      
-            
-			
+};
             
 			
             
