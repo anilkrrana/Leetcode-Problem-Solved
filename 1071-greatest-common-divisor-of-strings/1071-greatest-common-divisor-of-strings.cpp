25eb6c3fca870313d67268721c254d1afc494d09
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2023-02-01 19:45:16 +0530

    Time: 6 ms (50.18%), Space: 7 MB (88.75%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,29 +1,29 @@
-// class Solution {
-// public:
-//     string gcdOfStrings(string str1, string str2) {
-//         return (str1 + str2 == str2 + str1)? 
-//         str1.substr(0, gcd(size(str1),size(str2))): "";
-//     }
-// };
-
-
-
 class Solution {
 public:
-    int gcd(int num1,int num2){
-        if(num1 < num2) swap(num1, num2);
-        for(int i = num2; i >= 1; i--){
-            if(num1%i == 0 && num2 %i == 0) return i;
-        }
-        return 1;
-    }
     string gcdOfStrings(string str1, string str2) {
-        string ans;
-        if(str1 + str2 != str2 + str1) return "";
-        else{
-            int len = gcd(str1.length(), str2.length());
-            ans = str1.substr(0, len);
-        }
-        return ans;
+        return (str1 + str2 == str2 + str1)? 
+        str1.substr(0, gcd(size(str1),size(str2))): "";
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int gcd(int num1,int num2){
+//         if(num1 < num2) swap(num1, num2);
+//         for(int i = num2; i >= 1; i--){
+//             if(num1%i == 0 && num2 %i == 0) return i;
+//         }
+//         return 1;
+//     }
+//     string gcdOfStrings(string str1, string str2) {
+//         string ans;
+//         if(str1 + str2 != str2 + str1) return "";
+//         else{
+//             int len = gcd(str1.length(), str2.length());
+//             ans = str1.substr(0, len);
+//         }
+//         return ans;
+//     }
+// };
