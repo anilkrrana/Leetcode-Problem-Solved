c2ca061291f73929903dcde1aa8b23e1f6dfa3a7
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2023-02-26 22:24:39 +0530

    Time: 13 ms (66.46%), Space: 9.1 MB (23.71%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+    int m = word1.size();
+    int n = word2.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == 0) {
+                dp[i][j] = j;
+            }
+            else if (j == 0) {
+                dp[i][j] = i;
+            }
+            else if (word1[i - 1] == word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+            else {
+                dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+            }
+        }
+    }
+    return dp[m][n];
+ }
+};
+//The time complexity of this solution is O(mn), where m and n are the lengths of word1 and word2, respectively. The space complexity is also O(mn), as we are using a 2D table to store the subproblems.
+
+
+
+
