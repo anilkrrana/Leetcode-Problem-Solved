271a902c5b16f2706aba2e87a547f662a43d55bf
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2023-03-27 07:54:37 +0530

    Time: 65 ms (78.20%), Space: 42.6 MB (83.06%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.js
+++ 0064-minimum-path-sum/0064-minimum-path-sum.js
@@ -21,20 +21,3 @@ var minPathSum = function(grid) {
 
 
 
-
-// var minPathSum = function(grid) {
-//     for(let i = 1; i < grid[0].length; i++) {
-//         grid[0][i] += grid[0][i - 1]
-//     }
-//     for(let i = 1; i < grid.length; i++) {
-//         grid[i][0] += grid[i - 1][0]
-//     }
-    
-//     for(let i = 1; i < grid.length; i++) {
-//         for(let j = 1; j < grid[0].length; j++){
-//             grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]) 
-//         }
-//     }
-    
-//     return grid[grid.length - 1][grid[0].length - 1]
-// };
