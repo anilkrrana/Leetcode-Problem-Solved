73b9bf14ee39dc97c7dfbf15d73e2c9dc5ab6ad0
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2023-04-26 19:21:56 +0530

    Time: 105 ms (69.02%), Space: 52.8 MB (35.42%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -4,9 +4,20 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function(obj, classFunction) {
-    if(obj === null || obj === undefined || typeof classFunction !== 'function') return false
-    return Object(obj) instanceof classFunction
+    while(obj!=null)
+    {
+        if(obj.constructor === classFunction)
+        {
+            return true;
+        }
+
+        obj = Object.getPrototypeOf(obj);
+
+    }
+
+    return false;
 };
+
 
 /**
  * checkIfInstanceOf(new Date(), Date); // true
