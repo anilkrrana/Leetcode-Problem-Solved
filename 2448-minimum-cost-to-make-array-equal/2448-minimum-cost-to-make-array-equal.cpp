b54288618a0236f083d6ba3ea8f0464dbb02f259
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2023-06-21 21:23:41 +0530

    Time: 108 ms (60.22%), Space: 42.7 MB (33.58%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long minCost(vector<int>& nums, vector<int>& cost) {
+        int n = nums.size();
+        long long ans = 1e18;
+        vector<pair<int,int>> v(n);
+        for(int i = 0; i < nums.size(); i++)
+            v[i] = {nums[i], cost[i]};
+        
+        sort(v.begin(), v.end());
+        vector<long long> pref(n), suff(n);
+        long long c_sum = 0, nc = 0;
+        for(int i = 0; i < n; i++){
+            pref[i] = (v[i].first * c_sum - nc);
+            c_sum += (long long)v[i].second;
+            nc += (long long)v[i].first * (long long)v[i].second;
+        }
+        c_sum = 0, nc = 0;
+        for(int i = n - 1; i >= 0; i--){
+            ans = min(ans, abs(v[i].first * c_sum - nc) + pref[i]);
+            c_sum += (long long)v[i].second;
+            nc += (long long)v[i].first * (long long)v[i].second;
+        }
+        return ans;
+    }
+};
