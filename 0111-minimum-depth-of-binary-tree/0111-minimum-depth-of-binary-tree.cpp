1b82d139c0e3b8bd138cd42fade017443e75dd47
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2023-07-10 21:22:24 +0530

    Time: 328 ms (5.40%), Space: 144.9 MB (16.53%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,23 +12,16 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-            // Base case...
-            // If the subtree is empty i.e. root is NULL, return depth as 0...
-        if(root == NULL)  return 0;
-            // Initialize the depth of two subtrees...
-        int leftDepth = minDepth(root->left);
-        int rightDepth = minDepth(root->right);
-            // If the both subtrees are empty...
-        if(root->left == NULL && root->right == NULL)
-            return 1;
-            // If the left subtree is empty, return the depth of right subtree after adding 1 to it...
-        if(root->left == NULL)
-            return 1 + rightDepth;
-            // If the right subtree is empty, return the depth of left subtree after adding 1 to it...
-        if(root->right == NULL)
-            return 1 + leftDepth;
-            // When the two child function return its depth...
-            // Pick the minimum out of these two subtrees and return this value after adding 1 to it...
-        return min(leftDepth, rightDepth) + 1;    // Adding 1 is the current node which is the parent of the two                                                    subtrees...
+        if(root == NULL){
+            return 0;
+        }
+        
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+
+        if(root->left && root->right){
+            return min(left+1, right+1);
+        }
+        return max(left+1, right+1);
     }
 };
