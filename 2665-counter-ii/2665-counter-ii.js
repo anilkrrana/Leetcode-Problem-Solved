6ef0e770cbc39f12c9ab8763782748148d9dec63
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2023-09-23 22:53:35 +0530

    Time: 60 ms (51.50%), Space: 44.9 MB (23.82%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    i = init;
+    function increment(){
+        return ++init;
+    }
+    function decrement(){
+        return --init;
+    }
+    function reset(){
+        init = i;
+        return init;
+    }
+    return{increment, decrement, reset}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
