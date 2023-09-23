3f369b6806ecb84d369cc3295e716d98ac815307
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2023-09-23 22:55:03 +0530

    Time: 41 ms (96.12%), Space: 42.3 MB (6.55%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+  const transformedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    transformedArr[i] = fn(arr[i], i);
+  }
+
+  return transformedArr;
+};
